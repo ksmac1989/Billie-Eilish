6ecf679164e6720881b9516e2abfa1e14e98408d
Krisada Sittiapakij <ksmac1989@gmail.com>
2019-04-22 15:45:40 -0400

    Update(20190422-3.33am)
    Update(20190422-3.33am)

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,31 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+
+$('#search').keyup(function(){
+
+  var searchField = $('#search').val();
+  var myExp = new RegExp(searchField, "i");
+
+  $.getJSON('./json/ticket.json', function(data) {
+      var output = '<ul class = "searchresults">';
+      $.each(data, function(key, val){
+
+          if ((val.Date.search(myExp) != -1) ||  
+          (val.Location.search(myExp)!= -1) || 
+          (val.City.search(myExp)!= -1) || 
+          (val.Time.search(myExp) != -1)) {
+              output += '<li>';
+              output += '<h5>' + val.Date + '</h5>';
+              output += '<h6>' + 'Location: ' + val.Location + '</h6>';
+              output += '<h6>' + 'City/Country: ' + val.City + '</h6>';
+              output += '<h6>' + 'Status: ' + val.Status + '</h6>';
+              output += '</li>';
+          }
+
+      });
+      output += '</ul>';
+      $('#update').html(output);
+  });
+})
